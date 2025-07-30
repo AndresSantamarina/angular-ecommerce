e755ba414d33496689034c984c1098d4852430fa
Andres Santamarina <andresesantamarina@gmail.com>
2025-07-29 22:25:33 -0300

    computed signal practice

--- src/app/pages/cart/cart.ts
+++ src/app/pages/cart/cart.ts
@@ -1,16 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { cartService } from '../../services/cartService';
 import { CartItem } from "./cart-item/cart-item";
+import { OrderSummary } from "./order-summary/order-summary";
 
 @Component({
   selector: 'app-cart',
-  imports: [CartItem],
+  imports: [CartItem, OrderSummary],
   template: `
     <div class="p-6 flex flex-col gap-4">
       <h2 class="text-sxl">Shopping Cart</h2>
       @for (item of cartService.cart(); track item.id) {
         <app-cart-item [item]="item"/>
       }
+      <app-order-summary/>
     </div>
   `,
   styles: ``,
